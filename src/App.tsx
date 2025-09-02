e0c605d4e0c4cec8d57321d18432382059d00817
vikassrini <1rn20cs168.tharika@gmail.com>
2025-09-02 10:38:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
